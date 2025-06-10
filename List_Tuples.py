6a2cc366cd6a241c7621b9ccddc84504c359a397
aveshkh1 <aveshkharni35@gmail.com>
2025-06-10 13:19:47 +0530

    programs_updated

--- List_Tuples.py
+++ List_Tuples.py
@@ -1,38 +1,38 @@
 #List is a built in data type that is used to store the set of values
 #or it's order collection of items that are mutuble in nature
-# list=[55,66,88,99,56,34]
-#
-# print(type(list))
-# list.append(44)
-# print(list)
-# list.reverse()
-# print(list)
-# list.sort()
-# print(list)
-# list.sort(reverse=True)
-# print(list)
-# list.remove(44)
-# print(list)
-# list.pop(4)
-# print(list)
-# list.insert(4,55)
-# print(list)
-# movie1=input("Enter the first movie name:")
-# movie2=input("Enter the second movie name:")
-# movie3=input("Enter the 3rd movie name:")
-# movies=[]
-# movies.append(movie1)
-# movies.append(movie2)
-# movies.append(movie3)
-# print(movies)
-# list=[1,2,1]
-# list1=list.copy()
-# list.reverse()
-# print(list)
-# if(list1==list):
-#     print("Its palindrome")
-# else:
-#     print("It's not a palindrome")
+list=[55,66,88,99,56,34]
+
+print(type(list))
+list.append(44)
+print(list)
+list.reverse()
+print(list)
+list.sort()
+print(list)
+list.sort(reverse=True)
+print(list)
+list.remove(44)
+print(list)
+list.pop(4)
+print(list)
+list.insert(4,55)
+print(list)
+movie1=input("Enter the first movie name:")
+movie2=input("Enter the second movie name:")
+movie3=input("Enter the 3rd movie name:")
+movies=[]
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+print(movies)
+list=[1,2,1]
+list1=list.copy()
+list.reverse()
+print(list)
+if(list1==list):
+    print("Its palindrome")
+else:
+     print("It's not a palindrome")
 tup=("C","D","D","A","A","C","A","S")
 print(tup.count("A"))
 list=["C","D","D","A","A","C","A","S"]
--- dict_set.py
+++ dict_set.py
@@ -1,27 +1,27 @@
 #Dictionary is built-in data type that is used to store data in key value pairs
 #dicionary is unordered collection of items and they don't allow duplicate keys
-# dict={
-#     "student_names":["Avesh","Tanu","Aishu","Ammu","Gundu"],
-#     "marks":{
-#         "Puc":[75,78,77,67,55,23],
-#         "SSLC":[80,75,67,77,55],
-#         "Engeneering":[81,85,77,75,66]
-#     }
-# }
-# dict['Roll Number']=["1MV21EE007","1CD21CS194","1MV21EE008","1MV21EE009","1MV21EE010"]
-# print(dict)
-# print(dict["marks"]["Puc"])
-# print(dict.keys())
-# print(dict.values())
+dict={
+    "student_names":["Avesh","Tanu","Aishu","Ammu","Gundu"],
+    "marks":{
+        "Puc":[75,78,77,67,55,23],
+        "SSLC":[80,75,67,77,55],
+        "Engeneering":[81,85,77,75,66]
+    }
+}
+dict['Roll Number']=["1MV21EE007","1CD21CS194","1MV21EE008","1MV21EE009","1MV21EE010"]
+print(dict)
+print(dict["marks"]["Puc"])
+print(dict.keys())
+print(dict.values())
 #Set is unordered collection of items each element in the set is unique and non duplicate
-# dic={
-#     "table":["a piece of furniture","list of facts and figures"],
-#     "cat":"a small animal"
-#
-# }
-# print(dic["table"])
-# sets={"python","java","C++","python","javascript","java","python","java","C++","C"}
-# print(len(sets))
+dic={
+    "table":["a piece of furniture","list of facts and figures"],
+    "cat":"a small animal"
+
+}
+print(dic["table"])
+sets={"python","java","C++","python","javascript","java","python","java","C++","C"}
+print(len(sets))
 dict={}
 subject=input("Enter the subject name:")
 marks=int(input("Enter the marks:"))
